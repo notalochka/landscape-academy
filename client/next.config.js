8c757f8de43d2c212741a0c603bf87eb78af3c0d
Natalia Shepel <nashepel@gmail.com>
2025-10-09 23:36:46 +0300

    Fix Vercel deployment: remove static export

--- client/next.config.js
+++ client/next.config.js
@@ -4,11 +4,7 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
-  // Дозволяємо статичний експорт для деплою
-  output: 'export',
   trailingSlash: true,
-  // Відключаємо Image Optimization для статичного експорту
-  assetPrefix: '',
   generateEtags: false,
   poweredByHeader: false,
 }
